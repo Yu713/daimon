d00ba046484f9a86bb7d56df701a7ee7c7bafc86
Yu713 <44984333+Yu713@users.noreply.github.com>
2020-07-14 19:34:00 +0100

    edit

--- Assets/_scripts/ExerciseController.cs
+++ Assets/_scripts/ExerciseController.cs
@@ -6,59 +6,9 @@ using UnityEngine;
 
 public class ExerciseController : MonoBehaviour
 {
-
-    public UserProfile UserProfile; //data from users
-
-    //public static ExerciseController Instance;
-   // public List<ExerciseGen>; 
-    
-
-    private enum ExerciseOutputs //list all exercises
-    {
-        A1,
-        A14,
-        A15,
-        A21,
-        B,
-        B11,
-        B12,
-        B13,
-        B14STS,
-        B22,
-        B32,
-        B33,
-        B41,
-        B41Y,
-        B42,
-        B51,
-        B51CircleCrunch,
-        B52,
-        B62,
-        B72,
-        B82JumpingJacks,
-        B83,
-        B86,
-        B1230,
-        B4260,
-        B7230,
-        B8330,
-        B8630,
-        B62120,
-        Plank,
-        PushUp,
-    }
-
-   //weight (Obese) delete A3.1 B8.1 B8.2,B8.4, C1.1 in S1
-
-
-
-    public void Sections() //S1 S2 S3
-    {
-
-
-    }
-
+    //list of exercises; we remove them bit by bit
     public List<string> Exercises;
+
     public void Reset()
     {
         Exercises.Clear();
@@ -78,12 +28,12 @@ public class ExerciseController : MonoBehaviour
         Exercises.Add("B41Y");
         Exercises.Add("B42");
         Exercises.Add("B51");
-        Exercises.Add("B51CircleCrunch");
+        Exercises.Add("B51");
         Exercises.Add("B52");
         Exercises.Add("B61");
         Exercises.Add("B62");
         Exercises.Add("B72");
-        Exercises.Add("B82JumpingJacks");
+        Exercises.Add("B82");
         Exercises.Add("B83");
         Exercises.Add("B86");
         Exercises.Add("B1230");
@@ -97,48 +47,14 @@ public class ExerciseController : MonoBehaviour
         Exercises.Add("Upright");
     }
 
-    public List<ExerciseOutputs> ExcludeExercises(UserProfile questions)
-    {
-        switch (UserProfile)
-        {
-            case UserProfile.Name:
-                break;
-            case UserProfile.Gender:
-                break;
-            case UserProfile.Age:
-                break;
-            case UserProfile.weigh:
-                {
-                    if£¨UserProfiles£©{
-
-                    }
-                }
-
-
-
-        }
-    }
-   
-
-        
-     }
-
-
-    
-
-    
-    
-  
-
-    void Start()
+    public void Start()
     {
         Reset();
-       
-
     }
-	
-	void Remove(string what){
-		Exercises.Remove(what);
-	}
+    public void RemoveExercise(string what)
+    {
+        if (Exercises.Contains(what)) Exercises.Remove(what);
+    }
+    }
 
-}
+
